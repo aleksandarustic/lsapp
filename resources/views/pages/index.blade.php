46fc905a24e77f8df64a33d152e967804b4f18c9
aleksandar.ustic.pikto <aleksandar.ustic.pikto>
2018-06-05 15:29:31 +0200

    Interface update

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+
+    @extends('layouts.app')
+
+    @section('content')
+        <div class="jumbotron text-center">
+             <h1>Welcome to laravel</h1>
+             <p>This is laravel aplication from "The laravel from scrath" youtube series</p>
+             <p>
+                 <a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>
+                 <a href="/register" class="btn btn-success btn-lg" role="button">Register</a>
+            </p>
+        </div>
+    @endsection
+   
+
